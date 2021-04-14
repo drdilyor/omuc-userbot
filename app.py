2bae3fc62c99e6d001dd2680f4ec93230aa964bc
drdilyor <drdilyor@outlook.com>
2021-04-15 01:22:43 +0500

    No hello and no aloo

---
+++ app.py
@@ -0,0 +1,53 @@
+import re
+
+from pyrogram import Client, filters
+
+app = Client('drdilyor')
+
+omuc_group = (
+    filters.chat('uzbekcoderslive')
+    |filters.chat('fullstack_omuc')
+    |filters.chat('frontend_omuc')
+    |filters.chat(-512797745) # test group
+)
+super_user = 1044136353
+
+rules = []
+
+def reply_rule(regex, reply_message, max_per_day=3):
+    already_replied = set()
+
+    def callback(app, message):
+        user = message.from_user.id
+
+        if user not in already_replied or user == super_user:
+
+            if user != super_user:
+                callback.today += 1
+                if callback.today > callback.max_per_day:
+                    return
+
+            already_replied.add(user)
+            message.reply_text(reply_message)
+
+    callback.already_replied = already_replied
+    callback.max_per_day = max_per_day
+    callback.today = 0
+
+    rules.append(callback)
+    app.on_message(omuc_group & filters.regex(regex, re.IGNORECASE))(callback)
+
+re_ping = '\
+^alo+|\
+^kim bo+r|\
+^foo$'
+
+reply_rule(re_ping, '**Pong!!**')
+
+re_hello = '\
+^salom(?:\s+hammaga)?$|\
+^(?:as)?salomu?\s+ala[yi]kum$\
+'
+reply_rule(re_hello, '**Salom**')
+
+app.run()
