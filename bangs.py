2f56a1c839d824826da8398cfd8a2a3cd1e6ac9c
drdilyor <drdilyor@outlook.com>
2021-04-21 00:17:04 +0500

    Ḫ̴̍ȇ̷ͅć̷̭ö̶́͜m̴͔̅e̵̺͆s̸̡͛

--- bangs.py
+++ bangs.py
@@ -1,4 +1,5 @@
 from pyrogram import filters
+from zalgo import zalgo as Z̵̠̓a̸͉̅l̶̺̎g̵̞͝o̵͚̿
 
 from app import app
 
@@ -14,6 +15,32 @@ def command(name):
 def google(term):
     if term:
         return 'https://google.com/search?q=' + term.replace(' ', '+')
+
+zalgo = Z̵̠̓a̸͉̅l̶̺̎g̵̞͝o̵͚̿()
+zalgo.numAccentsUp = (3, 8)
+zalgo.numAccentsDown = (1, 3)
+zalgo.numAccentsMiddle = (0, 0)
+zalgo.maxAccentsPerLetter = 6
+
+@command('z')
+@command('zalgo')
+def zalgo_command(text):
+    if text:
+        # some zalgos are changing the width of the character on telegram
+        # and spaces are getting less visible
+        return zalgo.zalgofy(text)
+
+hecomes = Z̵̠̓a̸͉̅l̶̺̎g̵̞͝o̵͚̿()
+zalgo.numAccentsUp = (5, 15)
+zalgo.numAccentsDown = (3, 10)
+zalgo.numAccentsMiddle = (2, 7)
+zalgo.maxAccentsPerLetter = 20
+
+@command('hc')
+@command('hecomes')
+def zalgo_hecomes(text):
+    if text:
+        return hecomes.zalgofy(text)
 
 @app.on_message(filters.me & filters.text & filters.regex('^!(\w+)(?:\s+(.*))?$'))
 def on_command(app, message):
