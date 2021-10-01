d371433dcc6e25062dfff90b8dc789eded6b6c99
drdilyor <drdilyor@outlook.com>
2021-10-01 15:51:31 +0500

    ALLOW_MODS flag

--- filterer.py
+++ filterer.py
@@ -1,11 +1,14 @@
+import os
+
 from pyrogram import Client
 from pyrogram.types import Message
 
-from app import app, omuc_group
 import colorlist
-
-
+from app import app, omuc_group, mod_group
 from ranker import get_score
+
+ALLOW_MODS = os.environ.get('ALLOW_MODS', '')
+
 
 async def on_sus(_app: Client, message: Message, score: int):
     # default, logs to console
@@ -15,6 +18,8 @@ async def on_sus(_app: Client, message: Message, score: int):
 # noinspection PyShadowingNames
 @app.on_message(omuc_group)
 async def on_message(app: Client, message: Message):
+    if not ALLOW_MODS and await app.get_chat_member(mod_group, message.from_user.id):
+        return
     if message.text is None:
         return
     if await colorlist.on_message(app, message):
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import logging
 import asyncio
-import signal
+import logging
+import os
 
-from app import app
 import colorlist  # noqa
 import db  # noqa
 import filterer  # noqa
 import reporter  # noqa
+from app import app
 
 logging.basicConfig(level=logging.INFO)
 
 asyncio.get_event_loop().run_until_complete(db.init())
 asyncio.get_event_loop().run_until_complete(colorlist.init())
 
-on_exit = lambda _, __: asyncio.get_event_loop().create_task(db.close())  # noqa
-signal.signal(signal.SIGINT, on_exit)
-signal.signal(signal.SIGTERM, on_exit)
-
 app.run()
+os.system(f'kill -9 {os.getpid()}')
